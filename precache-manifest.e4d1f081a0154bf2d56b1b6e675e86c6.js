c4fc0af15fe890d2a1efa8c33e714216d0f7b2de
dhruvchawla102 <dhruvchawla102@gmail.com>
2020-10-03 10:45:39 +0530

    Updates

---
+++ precache-manifest.e4d1f081a0154bf2d56b1b6e675e86c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab42a5fe6cfa091c9346e01d0d52bfa",
+    "url": "/school/index.html"
+  },
+  {
+    "revision": "c65a6ffad0cf6cbf35c8",
+    "url": "/school/static/css/main.f25a9fae.chunk.css"
+  },
+  {
+    "revision": "d15c43d94e1cbd223258",
+    "url": "/school/static/js/2.20d6f8f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/school/static/js/2.20d6f8f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65a6ffad0cf6cbf35c8",
+    "url": "/school/static/js/main.dd18134b.chunk.js"
+  },
+  {
+    "revision": "21660328275e829daf55",
+    "url": "/school/static/js/runtime-main.dc61c361.js"
+  }
+]);
